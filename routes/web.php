a4038f65e1976fd91c8e366b8387151b42b53aa6
Vladyslav Tsyndrin <vladtzm@gmail.com>
2024-10-03 23:34:31 +0300

    Initial commit for car_blog

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\LabController;
+
+use App\Http\Controllers\AuthController;
+
+
+Route::prefix('lab1')->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
